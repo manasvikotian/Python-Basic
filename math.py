ca2730ca92b68772124e6a5c855117ae7ef87e18
Manasvi Kotian <80024209+manasvikotian@users.noreply.github.com>
2022-01-21 22:43:46 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,15 +1,61 @@
-# Add implementation
-def add(x,y):
-	return x+y;
+import numpy as np
+import math
 
-#Subtract implementation
-def subtract(x,y):
-    pass
 
-# Multiply implementation
-def multiply(x,y):
-	pass
+#a
+def sum_list(n):
+    list_1 = [i for i in range(n+1)]
+    return(sum(list_1))
 
-# Divide implementation
-def divide(x,y):
-    pass
+
+#b
+def sum_numpy(n):
+    return(np.sum([i for i in range(n+1)]))
+
+
+#c
+def print_to_number():
+    i=1
+    while(i<=10):
+        print ("i = " , i)
+        i = i+1
+
+
+#d
+def perfect_square(list_1):
+    new_list = []
+    for i in range(len(list_1)):
+        sr = math.sqrt(list_1[i])
+        if(sr - math.floor(sr) == 0):
+            new_list.append(list_1[i])
+    return(new_list)
+
+
+
+#e
+def generator (x):
+    sqr = x * x
+    return (sqr)
+
+#f
+def unittest():
+    print(sum_list(50))
+
+    print(sum_numpy(50))
+
+    print_to_number()
+
+    print(perfect_square([i for i in range(100) if i != 0]))
+
+    print(generator(5))        
+
+unittest()
+
+
+#g
+A = 529
+B = 256
+
+Sum = A + B
+print (Sum)
+
